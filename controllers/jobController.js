127db74fac207255c4d94d3ad4190117b379b302
nikhilmujeeb <nikhilmujeeb@gmail.com>
2024-11-19 01:36:19 +0530

    first commit

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -221,7 +221,10 @@ export const jobDashboard = async (req, res) => {
 export const getPendingJobs = async (req, res) => {
   try {
     const pendingJobs = await Job.find({ isApproved: false });
-    res.status(200).json({ message: 'Pending jobs retrieved successfully', jobs: pendingJobs });
+    res.status(200).json({
+      message: 'Pending jobs retrieved successfully',
+      jobs: pendingJobs,
+    });
   } catch (error) {
     console.error('Error fetching pending jobs:', error);
     res.status(500).json({ message: 'Failed to retrieve pending jobs' });
