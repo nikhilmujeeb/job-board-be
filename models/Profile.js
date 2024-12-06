e8d77b27d2bf325e759cfe9f920486af515a3de5
nikhilmujeeb <nikhilmujeeb@gmail.com>
2024-12-06 16:41:18 +0530

    first commit

--- models/Profile.js
+++ models/Profile.js
@@ -1,3 +1,5 @@
+import mongoose from 'mongoose';
+
 const profileSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   firstName: { type: String, required: true },
