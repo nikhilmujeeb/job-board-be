093a4385f270102fc0b84f526ea9b06f3468fc41
nikhilmujeeb <nikhilmujeeb@gmail.com>
2024-12-08 13:20:22 +0530

    first commit

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,12 +7,11 @@ const router = express.Router();
 router.get('/profile', authMiddleware, getUserProfile);
 
 router.get('/getJobsApplied', authMiddleware, async (req, res) => {
-    console.log("User ID from token:", req.user._id);  // Log the user ID
     try {
-      const appliedJobs = await Job.find({ applicants: req.user._id }).populate('company');
+      const appliedJobs = await Job.find({ applicants: req.user._id }).populate('company'); // Populate the company field
       res.json({ jobs: appliedJobs });
     } catch (error) {
-      console.error("Error fetching applied jobs:", error);  // Log the error
+      console.error("Error fetching applied jobs:", error);
       res.status(500).json({ message: 'Failed to fetch applied jobs' });
     }
   });  
